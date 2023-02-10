338460aed45d442c7b45be85b8a1f24177fbd79a
Joshua <105324950+JoshKeesee@users.noreply.github.com>
2023-02-10 23:16:11 +0000

    .

--- index.js
+++ index.js
@@ -108,6 +108,9 @@ io.on("connection", (socket) => {
 			player.dbId = socket.user.id;
       player.id = socket.id;
       delete players[socket.id];
+      if (player.inventory.includes("sword") || player.inventory.includes("pickaxe")) {
+        player.inventory = ["coal", "torch", "", "", "", "", "", ""];
+      }
 			socket.emit("playerData", player);
 		}
 	};
